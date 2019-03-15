416d6ad1cc8195ad6dfb31d39eb8fa6af47769b1
Ernie Val <erndval@gmail.com>
2019-03-15 23:42:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,14 +10,14 @@ def using_pop(array)
   array.pop
 end
 
-def pop_with_args(array, count)
-  array.pop(count)
+def pop_with_args(array)
+  array.pop(2)
 end
 
 def using_shift(array)
   array.shift
 end
 
-def shift_with_args(array, count)
-  array.shift(count)
+def shift_with_args(array)
+  array.shift(2)
 end
